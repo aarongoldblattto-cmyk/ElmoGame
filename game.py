afd8bd261efc1b06062351952191184db7151dbd
Aaron Goldblatt <aarongoldblatt@Aarons-MacBook-Air.local>
2025-10-21 22:23:51 -0400

    Use local image and remove requests dependency

--- game.py
+++ game.py
@@ -1,53 +1,55 @@
 import pygame
+import os
 import sys
-import io
-import requests
 
-times = 0
+def resource_path(relative_path):
+    """Get absolute path to assets (works with PyInstaller)."""
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
 
 pygame.init()
 
-url = "https://static.wikia.nocookie.net/muppet/images/5/5a/Elmo-elmo-elmo.jpg/revision/latest?cb=20110917000614"
-response = requests.get(url)
-image_bytes = io.BytesIO(response.content)
-image = pygame.image.load(image_bytes)
+# Load Elmo image from local assets folder
+image = pygame.image.load(resource_path("assets/elmo.png"))
 
-
-WIDTH,HEIGHT = 800, 600
+WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Tried to close the window " + str(times) + " times")
+pygame.display.set_caption("Tried to close the window 0 times")
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
 ELMO_WIDTH, ELMO_HEIGHT = WIDTH, HEIGHT
-
 image = pygame.transform.smoothscale(image, (ELMO_WIDTH, ELMO_HEIGHT))
 
-FLASH_INTERVAL_MS = 5000  
+FLASH_INTERVAL_MS = 5000
 next_toggle = pygame.time.get_ticks() + FLASH_INTERVAL_MS
 show_elmo = True
 
 clock = pygame.time.Clock()
 FPS = 60
-while True:
+times = 0
 
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             times += 1
             pygame.display.set_caption("Tried to close the window " + str(times) + " times")
             FLASH_INTERVAL_MS -= 100
 
-
     now = pygame.time.get_ticks()
     if now >= next_toggle:
         show_elmo = not show_elmo
         next_toggle = now + FLASH_INTERVAL_MS
-    screen.fill(BLACK)
+
+    screen.fill(BLACK if show_elmo else WHITE)
 
     if show_elmo:
         screen.blit(image, (0, 0))
-    else:
-        screen.fill(WHITE)
 
     pygame.display.flip()
+    clock.tick(FPS)
